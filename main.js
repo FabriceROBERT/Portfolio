b7549c13e5bbed5f8fbb56c8e72ae00f1a4bd7e1
Fabriciooo <fabricerob@yahoo.fr>
2022-06-03 04:27:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+jQuery(document).ready(function($) {
+
+	'use strict';
+
+    var top_header = $('.parallax-content');
+    top_header.css({'background-position':'center center'}); // better use CSS
+
+    $(window).scroll(function () {
+    var st = $(this).scrollTop();
+    top_header.css({'background-position':'center calc(50% + '+(st*.5)+'px)'});
+    });
+
+
+    $('body').scrollspy({ 
+        target: '.fixed-side-navbar',
+        offset: 200
+    })
+})
